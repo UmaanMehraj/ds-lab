b7fb8e6e79c602d3886dff743c9fb7574b985f84
UmaanMehraj <umaanmehraj5@gmail.com>
2025-10-16 09:50:32 +0530

    Update arrayStack

--- arrayStack.c
+++ arrayStack.c
@@ -1,19 +1,24 @@
 // Programme representing various array based stack operations!
 #include <stdio.h>
 
+#define MAX 100
+
 // Function declarations
 
 void push(int arr[], int *top, int data);
+void pop(int *top);
 void isEmpty(int top);
+void isFull(int top, int maxSize);
 
 int main()
 {
     int top = -1;
 
-    int stack[100];
+    int stack[MAX];
 
-    // push(stack, &top, 5);
+    push(stack, &top, 5);
     isEmpty(top);
+    isFull(top, MAX);
 
     // printf("%d", stack[top]);
 }
@@ -33,5 +38,23 @@ void isEmpty(int top)
     else
     {
         printf("Stack is not empty \n");
+    }
+}
+
+void pop(int *top)
+{
+    (*top)--;
+}
+
+void isFull(int top, int maxSize)
+{
+
+    if (top == maxSize - 1)
+    {
+        printf("Stack is full! \n");
+    }
+    else
+    {
+        printf("Stack is not full! \n");
     }
 }
