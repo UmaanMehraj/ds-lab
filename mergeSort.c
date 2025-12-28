668561930c741f0683ff56ec3e5375a585f9539d
UmaanMehraj <umaanmehraj5@gmail.com>
2025-12-28 14:27:21 +0530

    fix: merge sort

--- mergeSort.c
+++ mergeSort.c
@@ -1,8 +1,77 @@
-#include<stdio.h>
+#include <stdio.h>
 
+void merge(int arr[], int left, int mid, int right)
+{
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
 
-v
+    int leftArray[n1], rightArray[n2];
 
-int main(){
-  return 0;
+    for (i = 0; i < n1; i++)
+    {
+        leftArray[i] = arr[left + i];
+    }
+    for (j = 0; j < n2; j++)
+    {
+        rightArray[j] = arr[mid + 1 + j];
+    }
+
+    i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2)
+    {
+        if (leftArray[i] <= rightArray[j])
+        {
+            arr[k] = leftArray[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = rightArray[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = leftArray[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = rightArray[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right)
+{
+    int mid;
+    if (left < right)
+    {
+        mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main()
+{
+    int arr[] = {9, 8, 7, 4, 1, 2};
+    int size = sizeof(arr) / sizeof(int);
+
+    mergeSort(arr, 0, size - 1);
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
 }
